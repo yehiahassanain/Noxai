574c48a77ab0cb9e0cf14cdac402772983a33fe2
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2026-07-18 21:20:33 +0300

    fix: remove missing component imports

--- noxai/app/page.tsx
+++ noxai/app/page.tsx
@@ -1,6 +1,4 @@
 import HeroSection from "./components/HeroSection";
-import SolutionsSection from "./components/SolutionsSection";
-import SolutionSection2 from "./components/SolutionSection2";
 import SolutionSection3 from "./components/SolutionSection3";
 import ProcessSection from "./components/ProcessSection";
 import ContactSection from "./components/ContactSection";
